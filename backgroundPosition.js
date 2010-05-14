7daf602eff830bb4ce1de97f2f16a3980cecdb37
Erik Potter <epotter@truegames.com>
2010-05-14 13:45:50 -0500

    Supports arbitrary units

--- backgroundPosition.js
+++ backgroundPosition.js
@@ -8,23 +8,21 @@ $.fn.extend({
   backgroundPosition: function(kwargs) {
     if (isIE) {
       if (kwargs.x && kwargs.y) {
-        this.css('background-position-x', kwargs.x+'px')
-            .css('background-position-y', kwargs.y+'px')
+        this.css('background-position-x', kwargs.x)
+            .css('background-position-y', kwargs.y)
       } else if (kwargs.x) {
-        this.css('background-position-x', kwargs.x+'px')
+        this.css('background-position-x', kwargs.x)
       } else if (kwargs.y) {
-        this.css('background-position-y', kwargs.y+'px')
+        this.css('background-position-y', kwargs.y)
       }
       return [this.css('background-position-x'), this.css('background-position-y')]
     } else {
       if (kwargs.x && kwargs.y) {
-        this.css('background-position', kwargs.x+'px '+kwargs.y+'px')
+        this.css('background-position', kwargs.x+' '+kwargs.y )
       } else if (kwargs.x) {
-        // console.log(this[0])
-        // var lastx = this.css('background-position')
-        this.css('background-position', kwargs.x+'px '+String(this.css('background-position')).split(' ')[1])
+        this.css('background-position', kwargs.x+' '+String(this.css('background-position')).split(' ')[1])
       } else if (kwargs.y) {
-        this.css('background-position', String(this.css('background-position')).split(' ')[0]+' '+kwargs.y+'px')
+        this.css('background-position', String(this.css('background-position')).split(' ')[0]+' '+kwargs.y)
       }
       return String(this.css('background-position')).split(' ')
     }
